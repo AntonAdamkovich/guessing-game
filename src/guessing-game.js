d58b3b82b9274041f2ef298e555ed2f57d8fa8e4
AntonAdamkovich <anton.adamkovich@gmail.com>
2017-01-29 19:55:40 +0300

    guessing-game/feature: add full functionality of GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this._numbersList = [];
+    	this._middle = 0;
+    	this._min = 0;
+    	this._max = 0;
+    }
 
     setRange(min, max) {
-
+    	this._min = min;
+    	this._max = max;
+    	for(var i = this._min; i < this._max; i++){
+    		this._numbersList.push(i);
+    	}
     }
 
     guess() {
-
+    	this._middle = Math.round((this._max + this._min) / 2);
+    	return this._numbersList[this._middle];
     }
 
     lower() {
-
+    	this._max = this._middle;
     }
 
     greater() {
-
+    	this._min = this._middle;
     }
 }
 
